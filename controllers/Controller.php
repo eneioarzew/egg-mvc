76f23d7214cc2ce3dd029fa15bb5666a0d74d227
EneioArzew <eneioarzew@gmail.com>
2021-08-04 21:28:35 +0800

    Changed arguments to getResource function to accept entire route array.

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,6 +1,9 @@
 <?php
 class Controller {
-    function getResource($class, $function, $args) {
+    function getResource($route) {
+        $class = $route[0];
+        $function = $route[1];
+        $args = $route[2];
         $file_string = 'controllers/'.strtolower($class).'.php';
         if (!file_exists($file_string)) return null;
         include_once $file_string;
