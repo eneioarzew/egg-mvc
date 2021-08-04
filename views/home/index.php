ad9fa34d0840c0da757ac795acf176cda712bc80
EneioArzew <eneioarzew@gmail.com>
2021-08-05 03:24:57 +0800

    Resetted page

--- views/home/index.php
+++ views/home/index.php
@@ -1,3 +1,4 @@
+<?php include_once 'session.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,23 +7,5 @@
 </head>
 <body marginhorizontal>
     <h1>This is Egg MVC.</h1>
-    <script>
-        getResponse = function(url, callback) {
-            request = new XMLHttpRequest()
-            request.onreadystatechange = function() {
-                if (request.readyState == 4 && request.status == 200) {
-                    callback(request.responseText);
-                }
-            }
-            request.open('POST', url, true)
-            request.send()
-        }
-
-        function displayFromAPI(response) {
-            document.body.innerHTML = response
-        }
-
-        getResponse('http://localhost/egg-mvc/home/returninput/banana', displayFromAPI)
-    </script>
 </body>
 </html>
